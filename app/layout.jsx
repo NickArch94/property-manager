e3cedaa290b91542ae4c2f08eba167846f0ea404
NickArch94 <niok123messages@gmail.com>
2026-07-17 12:31:21 -0600

    First commit w/ underpinnings of Next.js project.

---
+++ app/layout.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import '@/assets/styles/globals.css'
+
+export const metadata = {
+    title: 'Property Manager Training | Discover the Best Practices for Property Management',
+    description: 'Learn the ins and outs of property management with our comprehensive training program. Gain valuable insights, tips, and strategies to excel in the property management industry.',
+    keywords: 'property management, training, best practices, property management industry, property management tips, property management strategies',
+}
+
+const MainLayout = ({children}) => {
+  return (
+    <html lang="en">
+      <body>
+      <div>{children}</div>
+      </body>
+    </html>
+  )
+}
+
+export default MainLayout

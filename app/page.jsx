c5c5fc8712b77a9d51a5df8518a03c15a07499d5
NickArch94 <niok123messages@gmail.com>
2026-07-23 14:56:44 -0600

    Added final touches of section highlighting in the Navbar and created the start of the first Hero section.

--- app/page.jsx
+++ app/page.jsx
@@ -1,11 +1,10 @@
+import Hero from '@/components/hero'
 import React from 'react'
-import Link from 'next/link'
 
 const HomePage = () => {
   return (
     <div>
-      <h1 className="text-3xl">Welcome to the Property Manager Training Program</h1>
-      <Link href="/properties" className="text-blue-500 hover:underline">Show Properties</Link>
+      <Hero />
     </div>
   )
 };

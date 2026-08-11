5568e388171683084c50662a90459e3221da8d54
NickArch94 <niok123messages@gmail.com>
2026-08-10 21:48:06 -0600

    More alterations to property header image component

--- app/properties/[id]/page.jsx
+++ app/properties/[id]/page.jsx
@@ -38,8 +38,6 @@ const PropertyPage = () => {
     <>
     {!loading && property && (<>
       <PropertyHeaderImage image={ property.images[0] } />
-      {/* <h1 className='text-3xl font-bold'>Property Page</h1>
-      <button onClick={() => window.location.href = '/'} className='bg-blue-700 p-2'>Go Home!</button> */}
     </>
   )}
   </>
